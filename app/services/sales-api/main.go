3885a8c8006b8e90a185259a629c38f7d9921665
Sonny Ochoa <0xsonny@gmail.com>
2022-12-28 13:42:05 -0800

    Added new package structure, along with a new main file.

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var build = "develop"
+
+func main() {
+	// Construct the application logger
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	// Construct the application logger
+	config := zap.NewProductionConfig()
+
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]any{
+		"service": service,
+	}
+
+	config.OutputPaths = []string{"stdout"}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
+}
