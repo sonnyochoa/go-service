336218c1043c0ce29d2ac90b70be25f9fc70b349
Sonny Ochoa <0xsonny@gmail.com>
2022-12-29 15:58:34 -0800

    Package web contains a small web framework extensions

---
+++ foundation/web/web.go
@@ -0,0 +1,59 @@
+// Package web contains a small web framework extensions
+package web
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"syscall"
+
+	"github.com/dimfeld/httptreemux/v5"
+)
+
+// A Handler is a type that handles a http request within our own little mini
+// framework.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+
+// App is the entrypoint into our application and what configures our context
+// object for each of our http handlers. Feel free to add any configuration
+// data/logic on this App struct.
+type App struct {
+	*httptreemux.ContextMux
+	shutdown chan os.Signal
+}
+
+// NewApp creates an App value that handle a set of routes for the application.
+func NewApp(shutdown chan os.Signal) *App {
+	return &App{
+		ContextMux: httptreemux.NewContextMux(),
+		shutdown:   shutdown,
+	}
+}
+
+// SignalShutdown is used to gracefully shut down the app when an integrity
+// issue is identified.
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
+}
+
+// Handle sets a handler function for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) Handle(method string, group string, path string, handler Handler) {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		// PRE CODE PROCESSING
+
+		if err := handler(r.Context(), w, r); err != nil {
+			// ERROR handling
+			return
+		}
+
+		// POST CODE PROCESSING
+	}
+
+	finalPath := path
+	if group != "" {
+		finalPath = "/" + group + path
+	}
+
+	a.ContextMux.Handle(method, finalPath, h)
+}
