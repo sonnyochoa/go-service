52f0bd1cb88be6d50282ff52d51bcb0e859c73ce
Sonny Ochoa <0xsonny@gmail.com>
2022-12-29 16:00:37 -0800

    A group of test handlers for user access.

---
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -0,0 +1,27 @@
+package testgrp
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+// Handlers manages the set of check endpoints.
+type Handlers struct {
+	Log *zap.SugaredLogger
+}
+
+// Test handler is for development.
+func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+
+	h.Log.Infow("readiness", "statusCode", http.StatusOK, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
+
+	return json.NewEncoder(w).Encode(status)
+}
