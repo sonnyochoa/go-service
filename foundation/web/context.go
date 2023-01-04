edc877cada7bf99d230f493998dd79d55e1f0481
Sonny Ochoa <0xsonny@gmail.com>
2023-01-04 13:11:37 -0800

    This provides some helper methods and values for dealing with context.

---
+++ foundation/web/context.go
@@ -0,0 +1,50 @@
+package web
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+type ctxKey int
+
+const key ctxKey = 1
+
+// Values represent state for each request.
+type Values struct {
+	TraceID    string
+	Tracer     trace.Tracer
+	Now        time.Time
+	StatusCode int
+}
+
+// GetValues returns the values from the context.
+func GetValues(ctx context.Context) (*Values, error) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return nil, errors.New("values missing from context")
+	}
+
+	return v, nil
+}
+
+// GetTraceID returns the trace id from the context.
+func GetTraceID(ctx context.Context) string {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return "00000000-0000-0000-0000-000000000000"
+	}
+	return v.TraceID
+}
+
+// SetStatusCode sets the status code back into the context.
+func SetStatusCode(ctx context.Context, statusCode int) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return
+	}
+
+	v.StatusCode = statusCode
+}
