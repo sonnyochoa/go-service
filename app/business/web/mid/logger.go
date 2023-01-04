5c5340f3cc44e0df8aa88645f8967149317a6668
Sonny Ochoa <0xsonny@gmail.com>
2023-01-04 12:55:28 -0800

    This contains our error definitions and middleware code. It also contains our middleware logger.

---
+++ app/business/web/mid/logger.go
@@ -0,0 +1,44 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/sonnyochoa/go-service/foundation/web"
+	"go.uber.org/zap"
+)
+
+// Logger ...
+func Logger(log *zap.SugaredLogger) web.Middleware {
+
+	m := func(handler web.Handler) web.Handler {
+
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			// If the context is missing this value, request the service to be
+			// shutdown gracefully
+			v, err := web.GetValues(ctx)
+			if err != nil {
+				return err // web.NewShutdownError("web value missing from context")
+			}
+
+			log.Infow("request started", "trace_id", v.TraceID, "method", r.Method,
+				"path", r.URL.Path, "remoteaddr", r.RemoteAddr)
+
+			// call the next handler
+			err = handler(ctx, w, r)
+
+			log.Infow("request completed", "trace_id", v.TraceID, "method", r.Method,
+				"path", r.URL.Path, "remoteaddr", r.RemoteAddr, "statuscode",
+				v.StatusCode, "since", time.Since(v.Now))
+
+			// return the error so it can be handled further up the chain
+			return err
+		}
+
+		return h
+	}
+
+	return m
+}
