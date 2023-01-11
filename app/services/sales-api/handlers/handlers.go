acaf1e9b8c01d3b8570269d8a314b38020440264
Sonny Ochoa <0xsonny@gmail.com>
2023-01-11 12:11:38 -0800

    Added db as a field in the server mux and mux config.

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http/pprof"
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/sonnyochoa/go-service/app/business/sys/auth"
 	"github.com/sonnyochoa/go-service/app/business/web/mid"
 	"github.com/sonnyochoa/go-service/app/services/sales-api/handlers/debug/checkgrp"
@@ -36,12 +37,13 @@ func StandardLibraryMux() *http.ServeMux {
 // debug application routes for the service. This bypassing the use of the
 // DefaultServerMux. Using the DefaultServerMux would be a security risk since
 // a dependency could inject a handler into our service without us knowing it.
-func Mux(build string, log *zap.SugaredLogger) http.Handler {
+func Mux(build string, log *zap.SugaredLogger, db *sqlx.DB) http.Handler {
 	mux := StandardLibraryMux()
 
 	cgh := checkgrp.Handlers{
 		Build: build,
 		Log:   log,
+		DB:    db,
 	}
 	mux.HandleFunc("/debug/readiness", cgh.Readiness)
 	mux.HandleFunc("/debug/liveness", cgh.Liveness)
@@ -54,6 +56,7 @@ type APIMuxConfig struct {
 	Shutdown chan os.Signal
 	Log      *zap.SugaredLogger
 	Auth     *auth.Auth
+	DB       *sqlx.DB
 }
 
 // APIMux constructs a http.Handler with all application routes defined.
